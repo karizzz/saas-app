dfeacf2afc0dbb307242dfea7c31f0d00f60ecac
kartik <kartiksaxena.wps@gmail.com>
2025-05-31 09:44:08 -0600

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Click to get started</Button>
+    </div>
+  )
+}
+
+export default Page
