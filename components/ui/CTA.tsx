4f3236c328d624ada2aa46c0cd1ffae228dc015a
kartik <kartiksaxena.wps@gmail.com>
2025-06-08 15:04:30 -0600

    home page done

--- components/ui/CTA.tsx
+++ components/ui/CTA.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import Image from 'next/image'
+
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <article className="cta-section">
+      <button className='btn-primary w-full justify-center bg-yellow-400 hover:bg-yellow-300  text-black font-bold'>
+
+        Start Learning your way
+
+      </button>
+      <h1>
+        Build A Personalize Learning Companion
+      </h1>
+      <h2>
+        Pick a name, subject, voice & personality -
+        and start learning through voice converstations that
+        feel natural and fun
+      </h2>
+      <Image src ="images/cta.svg" alt = "cta" width = {500} height = {500}/>
+      
+      
+      <button className = 'btn-primary w-full justify-center bg-red-400 hover:bg-red-300 hover:bg-black text-white font-bold'>
+        Build New Companion
+      </button>
+    </article>
   )
 }
 
