d44b5e8f46a3328c44a61e69b314e7444e713368
kartik <kartiksaxena.wps@gmail.com>
2025-06-30 16:38:47 -0600

    clerk auth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar"
+import { ClerkProvider } from "@clerk/nextjs";
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
   subsets: ["latin"],
@@ -20,9 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{ variables: { colorPrimary: "#3b82f6" } }}>
         <Navbar />
         {children}
-        </body>
-    </html>
+      </ClerkProvider>
+
+    </body>
+    </html >
   );
 }
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 import  NavItems  from './NavItems'
 /**
  * Navbar component
@@ -11,22 +13,29 @@ import  NavItems  from './NavItems'
 
 const Navbar = () => {
   return (
-    <nav className="navbar flex items ">
-      {/* Link to the home page */}
-      <Link href="/">
-        <div className="flex items-center gap-2.5 cursor-pointer">
-          {/* Logo image */}
-          <Image src="/images/logo.svg" alt="logo" width={47} height={45} />
-        </div>
-      </Link>
-
-      <div className = "flex items-center gap-8">
-        <NavItems />
-        <p>Sign In</p>
-        
-       
-      </div>
-    </nav>
+      <nav className="navbar">
+          <Link href="/">
+              <div className="flex items-center gap-2.5 cursor-pointer">
+                  <Image
+                      src="/images/logo.svg"
+                      alt="logo"
+                      width={46}
+                      height={44}
+                  />
+              </div>
+          </Link>
+          <div className="flex items-center gap-8">
+              <NavItems />
+              <SignedOut>
+                  <SignInButton>
+                      <button className="btn-signin">Sign In</button>
+                  </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                  <UserButton />
+              </SignedIn>
+          </div>
+      </nav>
   )
 }
 
